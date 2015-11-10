c6d6ff47aa4dc64fa6eed53da71d18292ebb5498
Takuya Okamoto <characterokamo@gmail.com>
2015-11-10 06:41:36 -0800

    implement function that maps a mem region the flag of which is the same as target vma has

--- cow_mem/cow_mem.c
+++ cow_mem/cow_mem.c
@@ -7,6 +7,7 @@
 #include <linux/sched/sysctl.h>
 #include <linux/list.h>
 #include <linux/mm.h>
+#include <linux/mmdebug.h>
 #include <linux/rwsem.h>
 #include <linux/slab.h>
 #include <linux/mman.h>
@@ -25,13 +26,92 @@
 #include <linux/rbtree.h>
 //#include <linux/autoconf.h>
 #include <uapi/linux/capability.h>
-
+#include <linux/mutex.h>
+#include <linux/rbtree_augmented.h>
+#include <asm-generic/atomic-long.h>
+#include <linux/preempt_mask.h>
+#include <linux/task_work.h>
+#include <linux/fsnotify.h>
+#include <linux/eventpoll.h>
+#include <uapi/asm-generic/fcntl.h>
+#include <linux/ima.h>
+#include <linux/stat.h>
+#include <linux/hugetlb_inline.h>
+#include <linux/cdev.h>
+#include <linux/kobject.h>
+#include <linux/kref.h>
+#include <linux/sysfs.h>
+#include <linux/preempt.h>
+#include <linux/percpu-defs.h>
+#include <linux/percpu_counter.h>
+#include <linux/mount.h>
+#include <linux/printk.h>
 
 #include "cow_mem.h"
 #define DEVICE_NAME "cow_monitor" 
+
 int sysctl_max_map_count = DEFAULT_MAX_MAP_COUNT;
 struct kmem_cache *vm_area_cachep;
 struct kmem_cache *policy_cache;
+struct kmem_cache *filp_cachep;
+
+extern struct file *shmem_file_setup(const char *name, loff_t size, unsigned long flags);
+
+struct mnt_namespace {
+  atomic_t                count;
+  unsigned int            proc_inum;
+  struct mount *  root;
+  struct list_head        list;
+  struct user_namespace   *user_ns;
+  u64                     seq;    /* Sequence number to prevent loops */
+  wait_queue_head_t poll;
+  u64 event;
+};
+
+struct mnt_pcp {
+  int mnt_count;
+  int mnt_writers;
+};
+
+struct mountpoint {
+  struct hlist_node m_hash;
+  struct dentry *m_dentry;
+  struct hlist_head m_list; 
+  int m_count;
+};      
+
+struct mount {
+  struct hlist_node mnt_hash;
+  struct mount *mnt_parent;
+  struct dentry *mnt_mountpoint;
+  struct vfsmount mnt;
+  union {
+    struct rcu_head mnt_rcu;
+    struct llist_node mnt_llist;
+  };
+  struct mnt_pcp __percpu *mnt_pcp;
+  struct list_head mnt_mounts;    /* list of children, anchored here */
+  struct list_head mnt_child;     /* and going through their mnt_child */
+  struct list_head mnt_instance;  /* mount instance on sb->s_mounts */
+  const char *mnt_devname;        /* Name of device e.g. /dev/dsk/hda1 */
+  struct list_head mnt_list;
+  struct list_head mnt_expire;    /* link in fs-specific expiry list */
+  struct list_head mnt_share;     /* circular list of shared mounts */
+  struct list_head mnt_slave_list;/* list of slave mounts */
+  struct list_head mnt_slave;     /* slave list entry */
+  struct mount *mnt_master;       /* slave is on master->mnt_slave_list */
+  struct mnt_namespace *mnt_ns;   /* containing namespace */
+  struct mountpoint *mnt_mp;      /* where is it mounted */
+  struct hlist_node mnt_mp_list;  /* list mounts with the same mountpoint */
+  struct hlist_head mnt_fsnotify_marks;
+  __u32 mnt_fsnotify_mask;
+  int mnt_id;                     /* mount identifier */
+  int mnt_group_id;               /* peer group identifier */
+  int mnt_expiry_mark;            /* true if marked for expiry */
+  struct hlist_head mnt_pins;
+  struct path mnt_ex_mountpoint;
+};
+  
 
 static int device_open(struct inode *inode, struct file *file)
 {
@@ -203,6 +283,199 @@ static inline int cow_mlock_future_check(struct mm_struct *mm,
   return 0;
 }
 
+const struct vm_operations_struct generic_file_vm_ops = {
+  .fault          = filemap_fault,
+  .map_pages      = filemap_map_pages,
+  .page_mkwrite   = filemap_page_mkwrite,
+  .remap_pages    = generic_file_remap_pages,
+};
+
+#define shmem_vm_ops generic_file_vm_ops
+
+int cow_shmem_zero_setup(struct vm_area_struct *vma)
+{
+  struct file *file;
+  loff_t size = vma->vm_end - vma->vm_start;
+
+  file = shmem_file_setup("dev/zero", size, vma->vm_flags);
+  if (IS_ERR(file))
+    return PTR_ERR(file);
+
+  /*  if (vma->vm_file)
+    fput(vma->vm_file); */
+  vma->vm_file = file;
+  vma->vm_ops = &shmem_vm_ops; //
+  return 0;
+}
+
+static void __vma_link_file(struct vm_area_struct *vma)
+{
+  struct file *file;
+
+  file = vma->vm_file;
+  if (file) {
+    struct address_space *mapping = file->f_mapping;
+        
+    if (vma->vm_flags & VM_DENYWRITE)
+      atomic_dec(&file_inode(file)->i_writecount);
+    if (vma->vm_flags & VM_SHARED)
+      atomic_inc(&mapping->i_mmap_writable);
+
+    flush_dcache_mmap_lock(mapping);
+    if (unlikely(vma->vm_flags & VM_NONLINEAR))
+      vma_nonlinear_insert(vma, &mapping->i_mmap_nonlinear);
+    else
+      vma_interval_tree_insert(vma, &mapping->i_mmap);
+    flush_dcache_mmap_unlock(mapping);
+  }                               
+}
+
+
+void __vma_link_list(struct mm_struct *mm, struct vm_area_struct *vma,
+		     struct vm_area_struct *prev, struct rb_node *rb_parent)
+{
+  struct vm_area_struct *next;
+
+  vma->vm_prev = prev;
+  if (prev) {
+    next = prev->vm_next;
+    prev->vm_next = vma;
+  } else {
+    mm->mmap = vma;
+    if (rb_parent)
+      next = rb_entry(rb_parent,
+		      struct vm_area_struct, vm_rb);
+    else
+      next = NULL;
+  }
+  vma->vm_next = next;
+  if (next)
+    next->vm_prev = vma;
+}
+
+
+static long vma_compute_subtree_gap(struct vm_area_struct *vma)
+{
+  unsigned long max, subtree_gap;
+  max = vma->vm_start;
+  if (vma->vm_prev)
+    max -= vma->vm_prev->vm_end;
+  if (vma->vm_rb.rb_left) {
+    subtree_gap = rb_entry(vma->vm_rb.rb_left,
+			   struct vm_area_struct, vm_rb)->rb_subtree_gap;
+    if (subtree_gap > max)
+      max = subtree_gap;
+  }               
+  if (vma->vm_rb.rb_right) {
+    subtree_gap = rb_entry(vma->vm_rb.rb_right,
+			   struct vm_area_struct, vm_rb)->rb_subtree_gap;  
+    if (subtree_gap > max)
+      max = subtree_gap;
+  }
+  return max;
+}
+
+#define validate_mm_rb(root, ignore) do {} while(0)
+#define validate_mm(mm) do {} while(0)
+
+RB_DECLARE_CALLBACKS(static, vma_gap_callbacks, struct vm_area_struct, vm_rb, unsigned long, rb_subtree_gap, vma_compute_subtree_gap)
+
+static void vma_gap_update(struct vm_area_struct *vma)
+{       
+  /*      
+   * As it turns out, RB_DECLARE_CALLBACKS() already created a callback
+   * function that does exacltly what we want.
+   */
+  vma_gap_callbacks_propagate(&vma->vm_rb, NULL);
+}
+
+static inline void vma_rb_insert(struct vm_area_struct *vma,
+                                 struct rb_root *root)
+{       
+  /* All rb_subtree_gap values must be consistent prior to insertion */
+  validate_mm_rb(root, NULL);
+
+  rb_insert_augmented(&vma->vm_rb, root, &vma_gap_callbacks);
+}
+
+void __vma_link_rb(struct mm_struct *mm, struct vm_area_struct *vma,
+		   struct rb_node **rb_link, struct rb_node *rb_parent)
+{
+  /* Update tracking information for the gap following the new vma. */
+  if (vma->vm_next)
+    vma_gap_update(vma->vm_next);
+  else
+    mm->highest_vm_end = vma->vm_end;
+                
+  /*
+   * vma->vm_prev wasn't known when we followed the rbtree to find the
+   * correct insertion point for that vma. As a result, we could not
+   * update the vma vm_rb parents rb_subtree_gap values on the way down.
+   * So, we first insert the vma with a zero rb_subtree_gap value
+   * (to be consistent with what we did on the way down), and then
+   * immediately update the gap to the correct value. Finally we
+   * rebalance the rbtree after all augmented values have been set.
+   */
+  rb_link_node(&vma->vm_rb, rb_parent, rb_link);
+  vma->rb_subtree_gap = 0;
+  vma_gap_update(vma);
+  vma_rb_insert(vma, &mm->mm_rb);
+}
+
+static void
+__vma_link(struct mm_struct *mm, struct vm_area_struct *vma,
+	   struct vm_area_struct *prev, struct rb_node **rb_link,
+	   struct rb_node *rb_parent)
+{
+  __vma_link_list(mm, vma, prev, rb_parent);
+  __vma_link_rb(mm, vma, rb_link, rb_parent);
+}
+
+
+static void vma_link(struct mm_struct *mm, struct vm_area_struct *vma,
+		     struct vm_area_struct *prev, struct rb_node **rb_link,
+		     struct rb_node *rb_parent)
+{
+  struct address_space *mapping = NULL;
+        
+  if (vma->vm_file) {
+    mapping = vma->vm_file->f_mapping;
+    mutex_lock(&mapping->i_mmap_mutex);
+  }
+                
+  __vma_link(mm, vma, prev, rb_link, rb_parent);
+  __vma_link_file(vma);
+
+  if (mapping)
+    mutex_unlock(&mapping->i_mmap_mutex);
+
+  mm->map_count++;
+  validate_mm(mm);
+}       
+
+
+static const char *gate_vma_name(struct vm_area_struct *vma)
+{
+  return "[vsyscall]";
+}
+static struct vm_operations_struct gate_vma_ops = {
+  .name = gate_vma_name,
+};
+static struct vm_area_struct gate_vma = {
+  .vm_start       = VSYSCALL_ADDR,
+  .vm_end         = VSYSCALL_ADDR + PAGE_SIZE,
+  .vm_page_prot   = PAGE_READONLY_EXEC,
+  .vm_flags       = VM_READ | VM_EXEC,
+  .vm_ops         = &gate_vma_ops,
+};
+
+struct vm_area_struct *get_gate_vma(struct mm_struct *mm)
+{
+  if (!mm || mm->context.ia32_compat)
+    return NULL;
+  return &gate_vma;
+}
+
 
 unsigned long cow_mmap_region(unsigned long addr, unsigned long len, vm_flags_t vm_flags, unsigned long pgoff)
 {
@@ -241,12 +514,36 @@ unsigned long cow_mmap_region(unsigned long addr, unsigned long len, vm_flags_t 
   INIT_LIST_HEAD(&vma->anon_vma_chain);
 
   if (vm_flags & VM_SHARED) {
-    ;
+    error = cow_shmem_zero_setup(vma);
+    if (error)
+      goto free_vma;
   }
-  
+  vma_link(mm, vma, prev, rb_link, rb_parent);
 
+  //perf. is it necessary?
+
+  //stat account
+  vm_stat_account(mm, vm_flags, NULL, len >> PAGE_SHIFT);
+
+  //
+  if (vm_flags & VM_LOCKED) {
+    if (!((vm_flags & VM_SPECIAL) || is_vm_hugetlb_page(vma) ||
+	  vma == get_gate_vma(current->mm)))
+      mm->locked_vm += (len >> PAGE_SHIFT);
+    else
+      vma->vm_flags &= ~VM_LOCKED;
+  }
+
+  vma->vm_flags |= VM_SOFTDIRTY;
+
+  vma_set_page_prot(vma);
+
+  return addr;
+ free_vma:
+  kmem_cache_free(vm_area_cachep, vma);
+  
  unacct_error:  
-  return 100;
+  return error;
 }
 
 unsigned long cow_do_mmap_pgoff(unsigned long addr, unsigned long len, unsigned long prot, unsigned long flags, unsigned long vm_flags, unsigned long pgoff)
@@ -272,7 +569,6 @@ unsigned long cow_do_mmap_pgoff(unsigned long addr, unsigned long len, unsigned 
     return -ENOMEM;    
   }
     
-
   addr = get_unmapped_area(NULL, addr, len, 0, flags);
   if (addr & ~PAGE_MASK)
     return addr;
@@ -384,6 +680,7 @@ int init_module(void)
   printk(KERN_INFO "try 'sudo mknod %s c %d 0'\n", DEVICE_FILE_NAME, MAJOR_NUM);
 
   vm_area_cachep = KMEM_CACHE(vm_area_struct, SLAB_PANIC);
+  filp_cachep = KMEM_CACHE(file, SLAB_PANIC);
   policy_cache = kmem_cache_create("numa_policy", sizeof(struct mempolicy), 0, SLAB_PANIC, NULL);
   return 0;
 }
@@ -393,6 +690,7 @@ void cleanup_module(void)
   unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
   kmem_cache_destroy(vm_area_cachep);
   kmem_cache_destroy(policy_cache);
+  kmem_cache_destroy(filp_cachep);
 }
 
 MODULE_LICENSE("GPL");
