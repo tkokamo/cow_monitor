598a3d4e674c6e45f0ec32577ee75e9bf053d932
Takuya Okamoto <characterokamo@gmail.com>
2015-12-03 01:09:25 -0800

    imple copy_pXd_pages

--- cow_mem/cow_mem.c
+++ cow_mem/cow_mem.c
@@ -1042,7 +1042,68 @@ static inline bool is_cow_mapping(vm_flags_t flags)
 
 int copy_pte_pages(struct mm_struct *dst_mm, struct vm_area_struct *dst_vm, struct mm_struct *src_mm, struct vm_area_struct *src_vm, pmd_t *src_pmd, unsigned long addr, unsigned long end, unsigned long *i)
 {
-  
+  static int copy_pte_range(struct mm_struct *dst_mm, struct mm_struct *src_mm,
+			    pmd_t *dst_pmd, pmd_t *src_pmd, struct vm_area_struct *vma,
+			    unsigned long addr, unsigned long end)
+  {
+    pte_t *orig_src_pte, *orig_dst_pte;
+    pte_t *src_pte, *dst_pte;
+    spinlock_t *src_ptl, *dst_ptl;
+    int progress = 0;
+    int rss[NR_MM_COUNTERS];
+    swp_entry_t entry = (swp_entry_t){0};
+
+  again:
+    init_rss_vec(rss);
+
+    dst_pte = pte_alloc_map_lock(dst_mm, dst_pmd, addr, &dst_ptl);
+    if (!dst_pte)
+      return -ENOMEM;
+    src_pte = pte_offset_map(src_pmd, addr);
+    src_ptl = pte_lockptr(src_mm, src_pmd);
+    spin_lock_nested(src_ptl, SINGLE_DEPTH_NESTING);
+    orig_src_pte = src_pte;
+    orig_dst_pte = dst_pte;
+    arch_enter_lazy_mmu_mode();
+
+    do {
+      /*
+       * We are holding two locks at this point - either of them
+       * could generate latencies in another task on another CPU.
+       */
+      if (progress >= 32) {
+	progress = 0;
+	if (need_resched() ||
+	    spin_needbreak(src_ptl) || spin_needbreak(dst_ptl))
+	  break;
+      }
+      if (pte_none(*src_pte)) {
+	progress++;
+	continue;
+      }
+      entry.val = copy_one_pte(dst_mm, src_mm, dst_pte, src_pte,
+			       vma, addr, rss);
+      if (entry.val)
+	break;
+      progress += 8;
+    } while (dst_pte++, src_pte++, addr += PAGE_SIZE, addr != end);
+
+    arch_leave_lazy_mmu_mode();
+    spin_unlock(src_ptl);
+    pte_unmap(orig_src_pte);
+    add_mm_rss_vec(dst_mm, rss);
+    pte_unmap_unlock(orig_dst_pte, dst_ptl);
+    cond_resched();
+
+    if (entry.val) {
+      if (add_swap_count_continuation(entry, GFP_KERNEL) < 0)
+	return -ENOMEM;
+      progress = 0;
+    }
+    if (addr != end)
+      goto again;
+    return 0;
+  }
 }
 
 
@@ -1057,7 +1118,7 @@ int copy_pmd_pages(struct mm_struct *dst_mm, struct vm_area_struct *dst_vm, stru
    
     if (pmd_none_or_clear_bad(src_pmd))
       continue;
-    if (copy_pte_pages(dst_mm, dst_vm, src_mm, src_vm, src_pgd, addr, next, i)) {
+    if (copy_pte_pages(dst_mm, dst_vm, src_mm, src_vm, src_pmd, addr, next, i)) {
       return -ENOMEM;
     }
   } while (src_pmd++, addr = next, addr != end, *i += ((unsigned long) 1 << PMD_SHIFT), (addr != end) && (*i < length));
@@ -1075,7 +1136,7 @@ int copy_pud_pages(struct mm_struct *dst_mm, struct vm_area_struct *dst_vm, stru
     if (pud_none_or_clear_bad(src_pud))
       continue;
     //
-    if (copy_pmd_pages()) {
+    if (copy_pmd_pages(dst_mm, dst_vm, src_mm, src_vm, src_pud, addr, next, i)) {
       return -ENOMEM;
     }
   } while (src_pud++, addr = next, *i += ((unsigned long) 1 << PUD_SHIFT), (addr != end) && (*i < length));
